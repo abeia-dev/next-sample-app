ac9cac097c22410216e17882163d87c8d5c0319f
Felipe Oliveira <felpd3v@gmail.com>
2023-06-11 16:55:00 -0300

    style: missing function ident

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,9 +15,9 @@ const HomePage = () => {
 			<hr />
 			<ul>
 				{users.map((user) => (
-				<li key={user.id}>
-					({user.id}) {user.name} / {user.age} anos
-				</li>
+					<li key={user.id}>
+						({user.id}) {user.name} / {user.age} anos
+					</li>
 				))}
 			</ul>
 		</>
